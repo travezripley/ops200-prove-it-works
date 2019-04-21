69834b5358e133019755909fd493092a91bd9673
Travez Ripley <theripleys@gmail.com>
2019-04-20 17:56:53 -0700

    adds app.jsx and starts the e2e test writing

---
+++ test/e2e/app.spec.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const expect = require('chai').expect;
+const path = require('path');
+const Nightmare = require('nightmare');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../dist')));
+
+const url = 'http://localhost:8888';
+
+const nightmare = new Nightmare();
+
+describe('End to End Tests', () => {
+    let httpServer = null;
+    let pageObject = null;
+
+    before((done) => {
+        httpServer = app.listen(8888);
+        done();
+    });
+
+    beforeEach(() => {
+        pageObject = nightmare.goto(url);
+    });
+
+    after((done) => {
+        httpServer.close();
+        done();
+    });
+
+    // This is where your code is going to go
+});
